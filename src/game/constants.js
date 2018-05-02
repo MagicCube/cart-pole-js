6ea7bd9e2eebe5c600df2d20301b3a27c6012b75
亨睿 <henry.lx@alibaba-inc.com>
2018-05-02 15:48:37 +0800

    Add constants

--- src/game/constants.js
+++ src/game/constants.js
@@ -1,4 +1,10 @@
 export const CANVAS_WIDTH = 1024;
 export const CANVAS_HEIGHT = 500;
 
-export const GROUND_HEIGHT = CANVAS_HEIGHT / 10;
+export const GROUND_HEIGHT = 20;
+
+export const CART_WIDTH = 50;
+export const CART_HEIGHT = 30;
+
+export const POLE_WIDTH = 10;
+export const POLE_HEIGHT = 200;
