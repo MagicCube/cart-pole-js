f1f6bbf9f44a2a544c98ce762e1a73d0a573c40a
亨睿 <henry.lx@alibaba-inc.com>
2018-05-04 10:01:43 +0800

    Add keyboard support

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import GameEnv from './env/GameEnv';
 
 import './index.less';
 
+let env = null;
+
 document.addEventListener('DOMContentLoaded', async () => {
-  const env = new GameEnv(document.getElementById('cp-game'));
+  env = new GameEnv(document.getElementById('cp-game'));
   window.env = env;
-  const result = await env.reset();
-  console.log(result);
+  await env.reset();
+});
+
+
+window.addEventListener('keydown', ({ key }) => {
+  switch (key.toLowerCase()) {
+    case 'arrowright':
+      env.step(1);
+      break;
+    case 'arrowleft':
+      env.step(-1);
+      break;
+    case 'r':
+      env.reset();
+      break;
+    default:
+      break;
+  }
 });
