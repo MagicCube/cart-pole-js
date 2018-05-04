2356d02145028ed376c700f7fc53bab89807d049
Henry Li <henry1943@163.com>
2018-05-04 20:35:32 +0800

    Support URL Params

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import GameEnv from './env/GameEnv';
-import Agent from './agents/RandomHillClimbingAgent';
 
 import './index.less';
 
 let env = null;
 
 function setup() {
-  // Initialize agent
-  const agent = new Agent();
-  agent.init();
-  // Initialize game environment
-  env = new GameEnv(document.getElementById('cp-game'));
-  env.agent = agent;
-  // Start loop
-  env.reset();
+  getAgentClass().then((Agent) => {
+    // Initialize agent
+    const agent = new Agent();
+    agent.init();
+    // Initialize game environment
+    env = new GameEnv(document.getElementById('cp-game'));
+    env.agent = agent;
+    // Start loop
+    env.reset();
+  });
+}
+
+function getAgentClass() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let agentName = urlParams.get('agent');
+  if (!agentName) {
+    agentName = 'Keyboard';
+  }
+  return import(`./agents/${agentName}Agent`).then(({ default: Agent }) => Agent );
 }
 
 document.addEventListener('DOMContentLoaded', setup);
