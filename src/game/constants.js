b1f479943e86b0a7cb798af91c205f2f66544686
Henry Li <henry1943@163.com>
2018-05-04 20:35:47 +0800

    Change CART_WIDTH

--- src/game/constants.js
+++ src/game/constants.js
@@ -3,11 +3,11 @@ export const CANVAS_HEIGHT = 360;
 
 export const GROUND_HEIGHT = 20;
 
-export const CART_WIDTH = 50;
+export const CART_WIDTH = 80;
 export const CART_HEIGHT = 30;
 
 export const POLE_WIDTH = 10;
 export const POLE_HEIGHT = 200;
 
 export const INITIAL_FORCE = 0.001;
-export const UNIT_FORCE = 0.0075;
+export const UNIT_FORCE = 0.01;
