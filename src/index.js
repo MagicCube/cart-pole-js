e15bdbf6d62dfbbb5c1bad6e551be8a82b5d9134
亨睿 <henry.lx@alibaba-inc.com>
2018-05-04 09:08:05 +0800

    Refactor: rename and move

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Game from './game/Game';
+
+import './index.less';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const game = new Game(document.getElementById('cp-game'));
+  window.game = game;
+  game.setup();
+  game.run();
+  game.restart();
+});
