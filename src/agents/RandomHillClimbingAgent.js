a2f0f6d1572fc15f0534c8e2d9fb4e77dc128953
Henry Li <henry1943@163.com>
2018-05-05 17:22:45 +0800

    Rename constant value name

--- src/agents/RandomHillClimbingAgent.js
+++ src/agents/RandomHillClimbingAgent.js
@@ -1,6 +1,6 @@
 import Agent from './Agent';
 
-const DIVERSITY = 0.1;
+const SHIFT_RATE = 0.1;
 
 export default class RandomHillClimbingAgent extends Agent {
   bestTotalReward = 0;
@@ -17,10 +17,10 @@ export default class RandomHillClimbingAgent extends Agent {
   onReset() {
     if (this.bestWeights) {
       this.weights = [
-        this.bestWeights[0] + (Math.random() * 2 - 1) * DIVERSITY,
-        this.bestWeights[1] + (Math.random() * 2 - 1) * DIVERSITY,
-        this.bestWeights[2] + (Math.random() * 2 - 1) * DIVERSITY,
-        this.bestWeights[3] + (Math.random() * 2 - 1) * DIVERSITY
+        this.bestWeights[0] + (Math.random() * 2 - 1) * SHIFT_RATE,
+        this.bestWeights[1] + (Math.random() * 2 - 1) * SHIFT_RATE,
+        this.bestWeights[2] + (Math.random() * 2 - 1) * SHIFT_RATE,
+        this.bestWeights[3] + (Math.random() * 2 - 1) * SHIFT_RATE
       ];
     } else {
       this.weights = [
@@ -32,9 +32,24 @@ export default class RandomHillClimbingAgent extends Agent {
       // Some of the best random weights found
       // during my tests.
       //
-      // this.weights = [ 0.12272906098273229, 0.05171682834523206, 0.7746913490599949, 1.109145679262887 ];
-      // this.weights = [ 0.22954301190558787, 0.00089575518357142, 0.5877188457671694, 0.40653325525236206 ]
-      // this.weights = [ 0.029339054268042605, 0.034424966244165224, 0.5089348031645287, 0.4829329102598643 ]
+      // this.weights = [
+      //   0.12272906098273229,
+      //   0.05171682834523206,
+      //   0.7746913490599949,
+      //   1.109145679262887
+      // ];
+      // this.weights = [
+      //   0.22954301190558787,
+      //   0.00089575518357142,
+      //   0.5877188457671694,
+      //   0.40653325525236206
+      // ];
+      // Kthis.weights = [
+      //   0.029339054268042605,
+      //   0.034424966244165224,
+      //   0.5089348031645287,
+      //   0.4829329102598643
+      // ];
     }
     this.totalReward = 0;
     console.info(this.weights.toString());
