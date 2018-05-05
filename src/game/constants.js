0eae9be87743918f82d34e9ca2930220825d8dbd
Henry Li <henry1943@163.com>
2018-05-05 18:58:38 +0800

    Change size

--- src/game/constants.js
+++ src/game/constants.js
@@ -3,11 +3,11 @@ export const CANVAS_HEIGHT = 240;
 
 export const GROUND_HEIGHT = 20;
 
-export const CART_WIDTH = 80;
+export const CART_WIDTH = 52;
 export const CART_HEIGHT = 30;
 
 export const POLE_WIDTH = 12;
-export const POLE_HEIGHT = 160;
+export const POLE_HEIGHT = 125;
 
 export const INITIAL_FORCE = 0.001;
 export const UNIT_FORCE = 0.02;
