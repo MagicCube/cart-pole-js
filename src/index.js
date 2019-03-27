002f148e65d98d448afb95ce180019fecb0b75ff
RexMa <Rex.magana@gmail.com>
2019-03-27 07:55:56 -0600

    Added switch statement in place of dynamic imports

--- src/index.js
+++ src/index.js
@@ -9,16 +9,14 @@ let env = null;
 window.tf = tf;
 
 function setup() {
-  getAgentClass().then((Agent) => {
-    // Initialize agent
-    const agent = new Agent();
-    agent.init();
-    // Initialize game environment
-    env = new GameEnv(document.getElementById('game'));
-    env.agent = agent;
-    // Start loop
-    env.reset();
-  });
+  const agent = getAgentClass();
+  // Initialize agent
+  agent.init();
+  // Initialize game environment
+  env = new GameEnv(document.getElementById('game'));
+  env.agent = agent;
+  // Start loop
+  env.reset();
 }
 
 function getAgentName() {
@@ -32,7 +30,22 @@ function getAgentName() {
 
 function getAgentClass() {
   const agentName = getAgentName();
-  return import(`./agents/${agentName}Agent`).then(({ default: Agent }) => Agent );
+  let agent;
+  switch (agentName) {
+    case 'Random':
+      agent = new RandomHillClimbingAgent();
+      break;
+    case 'Keyboard':
+      agent = new KeyboardAgent();
+      break;
+    case 'Policy':
+      agent = new PolicyAgent();
+      break;
+    default:
+      console.error('Unknown agent');
+  }
+
+  return agent;
 }
 
 document.addEventListener('DOMContentLoaded', setup);
